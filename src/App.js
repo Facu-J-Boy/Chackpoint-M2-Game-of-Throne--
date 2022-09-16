6994a246b14e24247d02d875e06508ebb0e912b1
Facu-J-Boy <boyfacundo45@gmail.com>
2022-09-16 09:39:48 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { Route } from "react-router-dom";
+import Houses from './components/Houses/Houses';
+import HouseDetail from './components/HouseDetail/HouseDetail';
+import CreateHouse from './components/CreateHouse/CreateHouse';
+import Nav from './components/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
-      HENRY GOT
+      <Nav />
+      <Route exact path='/' component={Houses} />
+      <Route exact path='/houses/:houseId' component={HouseDetail} />
+      <Route exact path='' component={CreateHouse} />
     </div>
   );
 }
